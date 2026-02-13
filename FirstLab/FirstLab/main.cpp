6707e27418c3a7acec9b7373fdabb995a3919314
Kovalyov Ruslan <i-am-kovalyov-ruslan@yandex.ru>
2026-02-13 18:22:42 +0300

    load and save image

--- FirstLab/FirstLab/main.cpp
+++ FirstLab/FirstLab/main.cpp
@@ -8,7 +8,13 @@ int main(int argc, char *argv[])
 {
     std::string s;
     QImage img;
-
+    for (int i = 0; i < argc; i++) {
+        if (!strcmp(argv[i], "-p") && (i + 1 <  argc)) {
+            s = argv[i + 1];
+        }
+    }
+    img.load(QString(s.c_str()));
+    img.save("C:/Users/black/Desktop/Images/img1.jpg");
     QCoreApplication app(argc, argv);
 
     return app.exec();
