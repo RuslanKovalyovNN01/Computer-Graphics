19c2111d65e132381e9c101a41c802f862c06919
Kovalyov Ruslan <i-am-kovalyov-ruslan@yandex.ru>
2026-02-13 20:02:37 +0300

    add realization of base class of filter

--- FirstLab/FirstLab/Filter.cpp
+++ FirstLab/FirstLab/Filter.cpp
@@ -1,1 +1,19 @@
 #include "Filter.h"
+
+template<typename T>
+T clamp(T value, T max, T min) {
+	if (value > max) return max;
+	if (value < min) return min;
+	return value;
+}
+
+QImage Filter::process(const QImage& img) const {
+	QImage result(img);
+	for (int x = 0; x < img.width(); x++) {
+		for (int y = 0; y < img.height(); y++) {
+			QColor color = calcNewPixelColor(img, x, y);
+			result.setPixelColor(x, y, color);
+		}
+	}
+	return result;
+}
